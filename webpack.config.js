fe8a2b33757f3f4365add2ce8bb4ef89516726b1
FaserParvez <FaserParvez@hotmail.com>
2018-12-14 01:00:51 +0000

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var Encore = require('@symfony/webpack-encore');
+
+Encore
+    .setOutputPath('public/build/')
+    .setPublicPath('/build')
+    .cleanupOutputBeforeBuild()
+    .autoProvidejQuery()
+    .autoProvideVariables({
+        "window.Bloodhound": require.resolve('bloodhound-js'),
+        "jQuery.tagsinput": "bootstrap-tagsinput"
+    })
+    .enableSassLoader()
+    .enableVersioning()
+    .addEntry('js/app', './assets/js/app.js')
+    .addStyleEntry('css/app', ['./assets/scss/app.scss'])
+    .splitEntryChunks()
+;
+
+module.exports = Encore.getWebpackConfig();
